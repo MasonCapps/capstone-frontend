6cf6d48d17db56b0e585890e9dbcb230df187d83
Mason Capps <masoncapps42119@gmail.com>
2022-07-14 14:47:05 -0500

    Adds final styling to home view

--- src/App.vue
+++ src/App.vue
@@ -32,72 +32,6 @@ export default {
 </script>
 
 <template>
-  <!-- <nav
-    v-if="
-      this.$route.path === `/sites/${this.localStorageSiteId}` ||
-      this.$route.path === '/orders-history' ||
-      this.$route.path === '/carted_services'
-    "
-  >
-    <router-link :to="`/sites/${this.localStorageSiteId}`">Home</router-link>
-    |
-    <router-link to="/carted_services">Cart</router-link>
-    |
-    <router-link to="/orders-history">Order History</router-link>
-    |
-    <div v-if="!isLoggedIn">
-      <router-link to="/signup-customer">Signup</router-link>
-      |
-      <router-link to="/login-customer">Login</router-link>
-    </div>
-    <router-link to="/logout-customer" v-if="isLoggedIn">Logout</router-link>
-  </nav>
-  <nav
-    v-if="
-      this.$route.path.slice(0, this.$route.path.lastIndexOf('/')) === '/sites/dashboard' ||
-      this.$route.path === '/orders' ||
-      this.$route.path === '/schedule'
-    "
-  >
-    <router-link to="/sites">Sites</router-link>
-    |
-    <router-link :to="`/sites/dashboard/${this.currentSiteId}`">Dashboard</router-link>
-    |
-    <router-link to="/orders">Orders</router-link>
-    |
-    <router-link to="/schedule">Schedule</router-link>
-    |
-    <router-link to="/logout">Logout</router-link>
-  </nav>
-  <nav
-    v-if="
-      this.$route.path.slice(0, this.$route.path.lastIndexOf('/')) !== '/sites/dashboard' &&
-      this.$route.path !== '/orders' &&
-      this.$route.path !== '/schedule' &&
-      this.$route.path !== '/sites/1' &&
-      this.$route.path !== '/signup-customer' &&
-      this.$route.path !== '/login-customer' &&
-      this.$route.path !== '/carted_services'
-    "
-  >
-    <div
-      id="link"
-      v-if="
-        isLoggedIn && this.$route.path !== `/sites/${this.localStorageSiteId}` && this.$route.path !== '/orders-history'
-      "
-    >
-      <router-link to="/sites">Sites</router-link>
-      |
-      <router-link to="/logout">Logout</router-link>
-    </div>
-    <div v-if="!isLoggedIn" id="link">
-      <router-link to="/">Home</router-link>
-      |
-      <router-link to="/signup">Signup</router-link>
-      |
-      <router-link to="/login">Login</router-link>
-    </div>
-  </nav> -->
   <!-- Sidebar -->
   <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
     <!-- Sidebar - Brand -->
@@ -637,7 +571,7 @@ export default {
                 aria-expanded="false"
               >
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small"></span>
-                <img class="img-profile rounded-circle" src="img/undraw_profile.svg" />
+                <img class="img-profile rounded-circle" src="/img/undraw_profile.svg" />
               </a>
               <!-- Dropdown - User Information -->
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,15 +16,28 @@ export default {
 
 <template>
   <div class="home">
-    <h1 class="text-gray-900">{{ message }}</h1>
+    <div class="py-5">
+      <h1 class="py-2 text-gray-900">{{ message }}</h1>
+      <h6>
+        <pre
+          style="font-family: Helvetica, Arial, sans-serif; margin-left: 50px"
+          class="text-primary col-lg-10 container text-gray-900"
+        >
+          The first <span style="color: #4e73df">business facing</span> and <span style="color: #4e73df">consumer facing</span> platform built directly for <span style="color: #4e73df">service businesses.</span>
+        </pre>
+      </h6>
+    </div>
     <br />
-    <div class="card shadow mb-4">
+    <div class="container py-3 card shadow mb-4 col-lg-10">
       <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">Basic Card Example</h6>
+        <h6 class="m-0 font-weight-bold text-primary">Mission</h6>
       </div>
       <div class="card-body">
-        The styling for this basic card example is created by using default Bootstrap utility classes. By using utility
-        classes, the style of the card component can be easily modified with no need for any custom CSS!
+        The biggest leap for a service business to make in order to scale to the size of their dreams is to grow a
+        virtual consumer base that requires less hands on maintenance. Our goal is to help service businesses take that
+        leap by allowing them to quickly create an online marketplace showcasing their services. Our hope is that this
+        tool will finally grant service business owners the power they need to take that next step into the world of
+        digital marketing.
         <p></p>
         <a href="/signup" class="btn btn-primary col-lg-4">
           <span class="icon text-white-50">
@@ -40,5 +53,9 @@ export default {
 <style>
 .home {
   text-align: center;
+}
+.container {
+  display: flex;
+  justify-content: center;
 }
 </style>
