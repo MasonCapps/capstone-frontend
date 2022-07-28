fb5e6a78b6b3d7d2e32aaab89bf737cf1a89ab99
Mason Capps <masoncapps42119@gmail.com>
2022-07-28 13:35:43 -0500

    Disables prettier for deployment

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://git.heroku.com/cryptic-ocean-11215.git/";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
