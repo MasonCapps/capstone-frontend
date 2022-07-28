02a955bc089627588c834cc18fb6fad61bf2689f
Mason Capps <masoncapps42119@gmail.com>
2022-07-28 14:46:51 -0500

    Updates backend request in main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import axios from "axios";
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://git.heroku.com/cryptic-ocean-11215.git/";
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://cryptic-ocean-11215.herokuapp.com/";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
